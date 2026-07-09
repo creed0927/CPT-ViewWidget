3d9608d1a097cbd9b903716b76dcf745f899e1b7
creed0927 <christian.reed0927@outlook.com>
2026-07-08 19:08:50 -0500

    Update shleem.js

--- shleem.js
+++ shleem.js
@@ -4,8 +4,8 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Pulls staging/loading data from CPT View and displays as a live widget
-// @match        *://yard-management.amazon.com/*
-// @match        *://*.yard-management.amazon.com/*
+// @match        *://*/*
+// @match        *://*/*
 // @updateURL    https://raw.githubusercontent.com/creed0927/CPT-ViewWidget/main/shleem.js
 // @downloadURL  https://raw.githubusercontent.com/creed0927/CPT-ViewWidget/main/shleem.js
 // @grant        GM_xmlhttpRequest
