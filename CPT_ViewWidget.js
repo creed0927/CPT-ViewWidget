ca19dcb8fe0ee1e9488298c0093a0e4fe5999a63
creed0927 <christian.reed0927@outlook.com>
2026-08-08 00:45:05 -0500

    Update CPT_ViewWidget.js

--- CPT_ViewWidget.js
+++ CPT_ViewWidget.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         CPT View Live Widget - OB Dock
 // @namespace    http://tampermonkey.net/
-// @version      5.6.5
+// @version      5.6.6
 // @description  Live CPT widget with local upload staging map
 // @match        *://*/*
 // @updateURL    https://raw.githubusercontent.com/creed0927/CPT-ViewWidget/refs/heads/main/CPT_ViewWidget.js
